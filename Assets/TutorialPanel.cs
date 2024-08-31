39f3f729cd8c8eb6b404a568e649ca215baa755b
Chandra Tritaqwa Ramadhan <107376090+Lolpotch@users.noreply.github.com>
2024-08-31 12:58:59 +0700

    Add files via upload

---
+++ Assets/TutorialPanel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPanel : MonoBehaviour
+{
+    PlayerMovement playerMovement;
+    Rigidbody2D playerRb;
+    SpawnManager spawnManager;
+
+    void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+        playerRb = playerMovement.GetComponent<Rigidbody2D>();
+        spawnManager= FindObjectOfType<SpawnManager>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            PlayerPrefs.SetInt("Tutorial", 1);
+            playerMovement.tutorial = 1;
+
+            playerRb.gravityScale = 3f;
+            FindObjectOfType<AudioManager>().StartMusic();
+            spawnManager.BeginSpawn();
+
+            gameObject.SetActive(false);
+        }
+    }
+}
